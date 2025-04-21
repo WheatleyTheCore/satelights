e8560dd57d24c15b6e2e92223b471f62929b05fc
WheatleyTheCore <alex.colwell@icloud.com>
2025-04-21 13:49:16 -0400

    north heading now calculated

--- displayController/Entities/Earth.py
+++ displayController/Entities/Earth.py
@@ -2,6 +2,7 @@ import pandas as pd
 from ursina import *
 import numpy as np
 import globals
+import json
 
 class Earth(Entity):
     def __init__(self, **kwargs):
@@ -25,5 +26,20 @@ class Earth(Entity):
         
     def update(self):
         if (globals.communicationQueue.qsize() > 0):
-            print(globals.communicationQueue.get())
+            magData = json.loads(globals.communicationQueue.get())
+            print(self.getMagnetometerAngle(magData))
+            
+    def getMagnetometerAngle(self, magData):
+        x, y, z = magData
+        angle = math.atan2(y, x)
+        if (angle >= 0):
+            angle = angle * (180 / math.pi)
+        else: 
+            angle = (angle + 2 * math.pi) * (180/math.pi)
+            
+        angle = math.floor(angle)
+        return angle - 90 if angle - 90 >= 0 else angle + 271
+        
+        
+
 
